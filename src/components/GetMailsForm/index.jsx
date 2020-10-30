2d16f33d04308db9b61bea1111033dc41d133c81
tojatos <tojatos@gmail.com>
2020-10-31 00:46:48 +0100

    Selectors for amount of mails added

--- src/components/GetMailsForm/index.jsx
+++ src/components/GetMailsForm/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 
 function GetMailsForm({setMails}) {
+    const [amount, setAmount] = useState(30);
+    const [shouldDownloadAll, setShouldDownloadAll] = useState(false);
     const { register, handleSubmit, errors } = useForm();
     const onSubmit = async ({login, password}) => {
+        const url = shouldDownloadAll ? 'https://krzysztofruczkowski.pl:2020/api/get_mails' : `https://krzysztofruczkowski.pl:2020/api/get_mails/${amount}`;
         try {
-            const result = await axios.post('https://krzysztofruczkowski.pl:2020/api/get_mails/30', {username: login, password: password});
+            const result = await axios.post(url, {username: login, password: password});
             if(Array.isArray(result.data)) {
                 let mails = result.data;
                 mails.forEach((e, i) => e.id = i);
@@ -19,6 +22,16 @@ function GetMailsForm({setMails}) {
         }
     }
 
+    const downloadAmount = () => {
+        setShouldDownloadAll(false);
+        handleSubmit();
+    };
+
+    const downloadAll = () => {
+        setShouldDownloadAll(true);
+        handleSubmit();
+    };
+
     return (
         <form className="get-mails-form" onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
@@ -29,9 +42,14 @@ function GetMailsForm({setMails}) {
                 <label htmlFor="password">Hasło</label>
                 <input name="password" type="password" ref={register({ required: true })} />
             </div>
+            <div className="form-group">
+                <label htmlFor="amount">Ilość maili (opcjonalnie)</label>
+                <input name="amount" type="number" value={amount} onChange={e => setAmount(Math.max(e.target.value, 0))}/>
+            </div>
             {errors.login && <span>Login wymagany</span>}
             {errors.password && <span>Hasło wymagane</span>}
-            <input type="submit" value="Pobierz maile" />
+            <button onClick={downloadAmount}>Pobierz maili: {amount}</button>
+            <button onClick={downloadAll}>Pobierz wszystkie maile</button>
         </form>
     );
 }
