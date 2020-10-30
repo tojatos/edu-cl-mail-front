32b3800bd49036c711251be41de63b6d41fa2d4a
tojatos <tojatos@gmail.com>
2020-10-30 15:19:40 +0100

    Priority filter added

--- src/components/Mailbox/index.jsx
+++ src/components/Mailbox/index.jsx
@@ -8,6 +8,7 @@ function Mailbox({mails}) {
     const [selectedMailId, setSelectedMailId] = useState(undefined);
     const [searchText, setSearchText] = useState("");
     const [selectedSenderOptions, setSelectedSenderOptions] = useState(undefined);
+    const [selectedPriorityOptions, setSelectedPriorityOptions] = useState(undefined);
 
     const onEmailListItemClick = clickedId => setSelectedMailId(selectedMailId === clickedId ? undefined : clickedId)
     const onSearchChange = event => setSearchText(event.target.value.toLowerCase());
@@ -17,12 +18,21 @@ function Mailbox({mails}) {
       .filter((x, i, a) => a.indexOf(x) === i) //unique
       .map(s => {return {value: s, label: s}});
 
+    const priorityOptions = mails
+      .map(mail => mail.priority)
+      .filter((x, i, a) => a.indexOf(x) === i) //unique
+      .map(s => {return {value: s, label: s}});
+
     const selectedMail = selectedMailId !== undefined ?
       mails.filter(e => e.id === selectedMailId).map(e => <Email {...e} />)[0] :
       <NoneSelected text="mail"/>;
 
     if(selectedSenderOptions && selectedSenderOptions.length > 0) {
       mails = mails.filter(m => selectedSenderOptions.includes(m.sender));
+    }
+
+    if(selectedPriorityOptions && selectedPriorityOptions.length > 0) {
+      mails = mails.filter(m => selectedPriorityOptions.includes(m.priority));
     }
 
     if(searchText.length > 0) {
@@ -35,6 +45,13 @@ function Mailbox({mails}) {
         defaultValue={selectedSenderOptions}
         onChange={v => setSelectedSenderOptions(v?.map(i => i.value))}
         options={senderOptions}
+        isMulti
+      />
+
+      <Select
+        defaultValue={selectedPriorityOptions}
+        onChange={v => setSelectedPriorityOptions(v?.map(i => i.value))}
+        options={priorityOptions}
         isMulti
       />
       <input type="text" placeholder="Szukaj w treści" onChange={onSearchChange} size="1" />
