81d2c2ca8f32e0a552192e3ee3fad2d46aadd63b
tojatos <tojatos@gmail.com>
2020-10-30 15:18:41 +0100

    Downloading dummy data added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from 'react';
 import GetMailsForm from '../GetMailsForm';
 import Mailbox from '../Mailbox';
+import axios from 'axios';
 
 function App() {
     const [mails, setMails] = useState([]);
+    const setDummyData = async () => {
+        const result = await axios.get('dummy_data.json');
+        setMails(result.data);
+    }
+
 
     return (
         <div>
             <GetMailsForm setMails={setMails}/>
+            <button onClick={setDummyData}>Pobierz dummy</button>
             {mails.length !== 0 ? <Mailbox mails={mails}/> : null}
         </div>
     );
