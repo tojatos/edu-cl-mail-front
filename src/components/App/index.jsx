310e8eb5a68c356b32da8d90d332cf827480e861
tojatos <tojatos@gmail.com>
2020-11-01 18:44:41 +0100

    Footer styles added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,15 +2,18 @@ import React, {useState} from 'react';
 import GetMailsForm from '../GetMailsForm';
 import Mailbox from '../Mailbox';
 import Footer from '../Footer';
+import './index.sass';
 
 function App() {
     const [mails, setMails] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     return (
-        <div>
-            <GetMailsForm setMails={setMails} setIsLoading={setIsLoading}/>
-            <Mailbox mails={mails} isLoading={isLoading}/>
+        <div className="app">
+            <div>
+                <GetMailsForm setMails={setMails} setIsLoading={setIsLoading}/>
+                <Mailbox mails={mails} isLoading={isLoading}/>
+            </div>
             <Footer/>
         </div>
     );
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Link from '../Link';
+import './index.sass'
 
 function Footer() {
     return (
-        <div>
+        <div className="footer">
             <span>Autor: <Link href='https://krzysztofruczkowski.pl' value='Krzysztof Ruczkowski'/></span>
             <span>Kod źródłowy frontendu: <Link href='https://github.com/tojatos/edu-cl-mail-front' value='link'/></span>
             <span>Kod źródłowy backendu: <Link href='https://github.com/tojatos/edu-cl-mail' value='link'/></span>
